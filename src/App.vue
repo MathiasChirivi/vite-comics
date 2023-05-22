3a74f6f98d57e15dd04dd226b02dd0963af1eabb
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-22 16:35:16 +0200

    add AppMainSeries

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script >
 import AppTitle from './components/AppTitle.vue'
 import AppMain from './components/AppMain.vue'
+import AppMainSeries from './components/AppMainSeries.vue'
 import AppMainComics from './components/AppMainComics.vue'
 import AppFooterImg from './components/AppFooterImg.vue'
 import AppFooterFollow from './components/AppFooterFollow.vue'
@@ -10,6 +11,7 @@ export default {
   components: {
     AppTitle,
     AppMain,
+    AppMainSeries,
     AppMainComics,
     AppFooterImg,
     AppFooterFollow
@@ -23,6 +25,7 @@ export default {
   </Header>
   <main>
     <AppMain />
+    <AppMainSeries />
     <AppMainComics />
   </main>
   <footer>
